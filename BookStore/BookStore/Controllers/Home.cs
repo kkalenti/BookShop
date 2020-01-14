86e3aab19ab7a7609d5b6477b9625685a2a1caaf
Калентьев Константин Анатольевич <kalentyev_ka@tomsk.lemz.ru>
2020-01-14 17:59:13 +0700

    Add book model and controllers with views

---
+++ BookStore/BookStore/Controllers/Home.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Book> _book;
+
+        public HomeController()
+        {
+            _book = new List<Book>
+            {
+                new Book()
+                {
+                    Title = "C# 8.0 and .NET Core 3.0 - Modern Cross-Platform Development",
+                    Description = "Learn the fundamentals, practical applications, and latest" +
+                                  " features of C# 8.0 and .NET Core 3.0 from expert teacher Mark J. Price.",
+                    Author = "Mark J. Price",
+                    PublishDate = "Oct, 2019",
+                    Price = 35.99,
+                    Image = "image1"
+                },
+                new Book()
+                {
+                    Title = "Pro ASP.NET Core MVC 2, 7th ed. Edition",
+                    Description = "Develop cloud-ready web applications using Microsoft's latest framework," +
+                                  "ASP.NET Core MVC 2",
+                    Author = "Adam Freeman",
+                    PublishDate = "October 25, 2017",
+                    Price = 20.79,
+                    Image = "image2"
+                }
+            };
+        }
+
+        /// <summary>
+        /// The home page
+        /// </summary>
+        /// <returns>Index view</returns>
+        public IActionResult Index()
+        {
+            return View(_book);
+        }
+
+        /// <summary>
+        /// The about page
+        /// </summary>
+        /// <returns>About view</returns>
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// The contact us page
+        /// </summary>
+        /// <returns>Contact view</returns>
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace BookStore.Models
+{
+    public class Book
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string PublishDate { get; set; }
+        
+        public double Price { get; set; }
+        
+        public string Image { get; set; }
+    }
+}
