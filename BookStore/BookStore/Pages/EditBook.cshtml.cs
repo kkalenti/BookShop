31e127db353d9d003f45608e130e6007f9219d46
Калентьев Константин Анатольевич <kalentyev_ka@tomsk.lemz.ru>
2020-04-26 18:03:49 +0700

    Add Razor Pages for books editing

---
+++ BookStore/BookStore/Pages/EditBook.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore
+{
+    public class EditBookModel : PageModel
+    {
+        private readonly BookStoreDbContext _context;
+
+        public EditBookModel(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+
+            Book = await _context.Books.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Book == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(Book.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("Index", "Home");
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BookStore/BookStore/Pages/ListBooks.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages
+{
+    public class ListBooksModel : PageModel
+    {
+        private readonly BookStoreDbContext _context;
+
+        public ListBooksModel(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Book> Books { get; set; }
+
+        public void OnGet()
+        {
+            Books = _context.Books.ToList();
+        }
+    }
+}
